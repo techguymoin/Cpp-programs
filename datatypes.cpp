c2d06764d96d29b37cfe136fc858c2c11d11c8b4
MOIN <syedmoin.tech@gmail.com>
2026-08-13 22:12:20 +0530

    datatypes in c++

---
+++ datatypes.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    short d=100;
+    long e=10;
+    long long f=355677;
+    string name="moin";
+    float decimal=1.23;
+    double g=1.234;
+    long double h=1.2345;
+
+    cout<<d<<endl;
+    cout<<e<<endl;
+    cout<<e<<endl;
+    cout<<f<<endl;
+    cout<<name<<endl;
+    cout<<decimal<<endl;
+    cout<<g<<endl;
+    cout<<h<<endl;
+}
