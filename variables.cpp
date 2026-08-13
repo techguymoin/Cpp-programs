a6928e3ccb8fc8190f02a750c7d4d54d6eb5ef13
MOIN <syedmoin.tech@gmail.com>
2026-08-13 18:42:27 +0530

    variables in c

---
+++ variables.cpp
@@ -0,0 +1,15 @@
+//variables can start from a letter or an underscore but we can write it later by in the name
+#include<iostream>
+using namespace std;
+int main(){
+    int _moin018=10,moin=10;
+    cout<<"the value of _moin018 is "<<_moin018<<endl; //printing with the string and variable
+     _moin018=100;
+         cout<<"the value of _moin018 is "<<_moin018; //we can reassign value to a variable
+         //starting with a letter
+          cout<<"the value of moin is "<<moin<<endl; //printing with the string and variable
+           _moin018=100;
+         cout<<"the value of _moin is "<<moin; //we can reassign value to a variable
+         return 0;
+}
+        
