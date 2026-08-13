7d370fc2be391896f6a97174c1b363c51a81a83f
MOIN <syedmoin.tech@gmail.com>
2026-08-13 22:35:08 +0530

    typecasting in c++

---
+++ typecasting.cpp
@@ -0,0 +1,9 @@
+#include<iostream> //preprocesser
+using namespace std;
+int main(){//function
+  int a=10;
+  int b=1.2;
+  cout<<(float) a/3<<endl;//converting int to float
+  cout<<(int) b;//converting float to int  
+    return 0;
+}
