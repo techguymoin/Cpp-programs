e39bae18181b1e4ea8cd4bd65dbdc3b29deb5d65
MOIN <syedmoin.tech@gmail.com>
2026-08-14 00:03:17 +0530

    while loop

---
+++ while_loop.cpp
@@ -0,0 +1,11 @@
+#include<iostream> //preprocesser
+using namespace std;
+int main(){//function
+    int index=0;
+
+    while(index<10){
+        cout<<"we are at index number "<<index<<endl;
+        index=index + 1;
+    }
+    return 0;
+}
