f4061ef20322117e1d7b6656220e4cbc0fcd30e5
MOIN <syedmoin.tech@gmail.com>
2026-08-14 00:13:14 +0530

    for loop

---
+++ for_loop.cpp
@@ -0,0 +1,13 @@
+#include <iostream> //preprocesser
+using namespace std;
+int main()
+{ // function
+    int i, index = 0;
+    for (i = 0; i < 10; i++)
+    {
+        index = i;
+        cout << "\nwe are at index number " << index;
+    }
+
+    return 0;
+}
