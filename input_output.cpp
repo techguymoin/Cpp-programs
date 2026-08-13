22b5089e8cf381bb94b0635a170b40337db09467
MOIN <syedmoin.tech@gmail.com>
2026-08-13 22:26:03 +0530

    Basic input output in c++

---
+++ input_output.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //For taking input we use cin with >> 
+    //for output we use cout amd <<
+    int a;
+    cout<<"Enter a number :";
+    cin>>a;
+    cout<<"\nThe number you gave is "<<a;
+    return 0;
+}
