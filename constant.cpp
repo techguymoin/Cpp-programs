425718cb4a13ec5b6cbc14d1953234847cb63958
MOIN <syedmoin.tech@gmail.com>
2026-08-13 23:18:57 +0530

    constant in c++

---
+++ constant.cpp
@@ -0,0 +1,13 @@
+#include<iostream> //preprocesser
+using namespace std;
+int main(){//function
+     //constants in c++
+    const int a=10;
+    cout<<"the value of a is "<<a;
+    //now its value cannot be changed
+    /*if we run this lines
+     int a=45;
+     cout<<"now the value of a is "<<a;
+     it will give a error      */
+    return 0;
+}
