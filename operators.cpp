63143b018578ab9ee89c0e670b52f68a25eb0302
MOIN <syedmoin.tech@gmail.com>
2026-08-13 23:32:52 +0530

    operators in c++

---
+++ operators.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int first_number,second_number;
+    cout<<"enter your first number: ";
+    cin>>first_number;
+    cout<<"enter your second number: ";
+    cin>>second_number;
+    cout<<"your first number is "<<first_number<<endl;
+    cout<<"your second number is "<<second_number<<endl;
+    cout<<"and the sum is "<<first_number+second_number<<endl;
+    cout<<"and the difference is "<<first_number-second_number<<endl;
+    cout<<"and the product is "<<first_number*second_number<<endl;
+    cout<<"and the division is "<<first_number/second_number<<endl;
+
+    return 0;
+}
