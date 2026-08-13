b513a99cb970c9953bce82cd793892d95e9178d0
MOIN <syedmoin.tech@gmail.com>
2026-08-13 22:11:37 +0530

    comments in c++

---
+++ comments.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"comments are returned using // for single line comment and /* */"<<endl;
+    cout<<"Example";
+    //this is an example of single line comment
+    /*this is an example of
+    multi line comment in c++*/
+
+}
