b112b3bfe3ea517855adfb49541289a509a3fd45
MOIN <syedmoin.tech@gmail.com>
2026-08-20 10:20:36 -0700

    Getting age and deciding who can vote

---
+++ voters.cpp
@@ -0,0 +1,15 @@
+#include<iostream> //preprocesser
+using namespace std;
+int main(){//function
+    int age;
+   cout<<"enter you age below"<<endl;
+    cin>>age;
+    if(age>=18){
+        cout<<"you can vote";
+    }
+    else{
+        cout<<"you are a under age person try After turning 18";
+    }
+
+    return 0;
+}
